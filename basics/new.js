fc33b42a9524a4710bad97f7108f4e5cf316f675
RISHABH PATEL <125333962+Patelrishabh99@users.noreply.github.com>
2024-10-05 16:32:02 +0000

    new basics of let and var

--- basics/new.js
+++ basics/new.js
@@ -1,1 +1,8 @@
 console.log("hello javascript")
+const accountId = 12776
+let accountName = "rishabh patel"
+var accountState = "Andhara pradesh"
+let accountCity ;
+
+accountState = "Bihar"
+console.table([accountId , accountName , accountState , accountCity])
