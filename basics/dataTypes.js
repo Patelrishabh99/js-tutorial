599089239230ed9dc31d50c30e17804141e0b7d5
RISHABH PATEL <125333962+Patelrishabh99@users.noreply.github.com>
2024-10-08 12:46:51 +0000

    object summary

--- basics/dataTypes.js
+++ basics/dataTypes.js
@@ -1,1 +1,34 @@
 "use strict";
+// console.log(typeof 12.53627);
+// console.log(typeof null)
+// console.log(typeof undefined)
+
+//convertion and operation
+
+let someNum = 99
+let convertedNum = String(someNum);
+// console.log(typeof convertedNum);
+// console.log(convertedNum);
+
+
+//**********************OPERATION********************************* *
+let value = 100
+let negValue = -value
+// console.log(negValue);
+
+
+//+++++++++++++++++++++++++++++OBJECTS++++++++++++++++++++++++++++
+
+let user1 = {
+    email : "rishabh@gmail.com",
+    id : "rishabh",
+}
+
+let user2 = user1;
+
+user2.email = "patel@gmail.com"
+console.log(user1.email);
+console.log(user2.email);
+
+// stack (primitive)     ---------------------   heap  (non primitive)
+
