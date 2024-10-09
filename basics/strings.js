752a4cf2c9408187ccbc01c4fad4372d336ceabb
RISHABH PATEL <125333962+Patelrishabh99@users.noreply.github.com>
2024-10-09 13:13:23 +0000

    strings updated

---
+++ basics/strings.js
@@ -0,0 +1,20 @@
+let myName = "Rishabh"
+let couponCount = 727
+
+console.log(`hello , my name is ${myName} and my coupon ccount is ${couponCount}`);
+
+const game = new String('gvpcdpgc')
+// console.log(game[5]);
+// console.log(game.__proto__);
+// console.log(game.length);
+// console.log(game.toUpperCase());
+// console.log(game.charAt(5));
+
+
+// use to remove the white space characters
+let firstString = "         Mahesh      .     ";
+console.log(firstString)
+console.log(firstString.trim());
+
+
+
