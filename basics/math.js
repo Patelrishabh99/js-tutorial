ac8242cbfe4af458aea4530c1f902c60ab8d28f3
RISHABH PATEL <125333962+Patelrishabh99@users.noreply.github.com>
2024-10-10 09:29:57 +0000

    math file commited

---
+++ basics/math.js
@@ -0,0 +1,26 @@
+const balance = new Number(6000);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// to make a any number as a precision 
+const preNumber = 896.4532;
+// console.log(preNumber.toPrecision(3));   
+// it will give the three digit precision.....
+
+const hundred = 20000000;
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+
+//++++++++++++++++++++++=MATHS+++++++++++++++++++++++++++++log
+// console.log(Math.abs(-20));    
+// console.log(Math.abs(12));
+// console.log(Math.round(20.67));
+
+// console.log(Math.ceil(34.23));
+// console.log(Math.floor(4.9))
+
+//random in math
+console.log(Math.random());
+console.log(Math.random()*100);
+
