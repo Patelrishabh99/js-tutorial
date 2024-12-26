c24226c2ec14e2c067f3017b23239d753acffcec
RISHABH PATEL <125333962+Patelrishabh99@users.noreply.github.com>
2024-12-26 04:55:02 +0000

    math random function

--- basics/math.js
+++ basics/math.js
@@ -1,11 +1,11 @@
 const balance = new Number(6000);
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(2));
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
 
 // to make a any number as a precision 
 const preNumber = 896.4532;
-// console.log(preNumber.toPrecision(3));   
-// it will give the three digit precision.....
+console.log(preNumber.toPrecision(3));   
+//it will give the three digit precision.....
 
 const hundred = 20000000;
 // console.log(hundred.toLocaleString('en-IN'));
@@ -13,14 +13,21 @@ const hundred = 20000000;
 
 
 //++++++++++++++++++++++=MATHS+++++++++++++++++++++++++++++log
-// console.log(Math.abs(-20));    
-// console.log(Math.abs(12));
-// console.log(Math.round(20.67));
+console.log(Math.abs(-20));    
+console.log(Math.abs(12));
+console.log(Math.round(20.67));
 
-// console.log(Math.ceil(34.23));
-// console.log(Math.floor(4.9))
+console.log(Math.ceil(34.23));
+console.log(Math.floor(4.9))
 
 //random in math
 console.log(Math.random());
 console.log(Math.random()*100);
 
+
+// writing  a  small code of formula to generate the random values between the max and min
+const min = 10;
+const max = 20;
+
+console.log(" hello in the function ::");
+console.log(Math.floor(Math.random()* (max - min + 1)) + min );
